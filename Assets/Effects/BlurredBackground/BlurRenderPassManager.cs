2f8038ee8ddc2c088edcda7010360f679557415a
WoBok <darwin.vinci.wkc@gmail.com>
2024-01-25 07:50:38 +0800

    pass settings

--- Assets/Effects/BlurredBackground/BlurRenderPassManager.cs
+++ Assets/Effects/BlurredBackground/BlurRenderPassManager.cs
@@ -1,10 +1,61 @@
-public class BlurRenderPassManager//出于性能考虑，所有的图片只能使用相同的Pass处理背景模糊，Pass有默认值，可以添加一个Mono脚本，通过Instance管理所有的参数
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class BlurRenderPassManager
 {
     static BlurRenderPassManager m_Instance;
     public static BlurRenderPassManager Instance { get { if (m_Instance == null) { m_Instance = new BlurRenderPassManager(); } return m_Instance; } }
 
-    public void Register(BlurRenderPass pass)
-    {
+    BlurRenderPass m_BlurRenderPass;
+    public BlurRenderPass BlurRenderPass { get { if (m_BlurRenderPass == null) { m_BlurRenderPass = new BlurRenderPass(); } return m_BlurRenderPass; } }
 
+    List<BlurredBackground> m_BlurredBackgrounds = new List<BlurredBackground>();
+    bool m_IsPassRunning;
+
+    public void Register(BlurredBackground background)
+    {
+        if (background == null) return;
+        m_BlurredBackgrounds.Add(background);
+        ProcessPass();
+    }
+    public void Unregister(BlurredBackground background)
+    {
+        if (background == null) return;
+        if (!m_BlurredBackgrounds.Contains(background)) return;
+        m_BlurredBackgrounds.Remove(background);
+        ProcessPass();
+    }
+    void ProcessPass()
+    {
+        if (m_BlurredBackgrounds.Count > 0)
+        {
+            if (m_IsPassRunning) return;
+            m_IsPassRunning = true;
+            RenderPipelineManager.beginCameraRendering += beginCameraRendering;
+        }
+        else
+        {
+            m_IsPassRunning = false;
+            RenderPipelineManager.beginCameraRendering -= beginCameraRendering;
+        }
+    }
+    void beginCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        if (camera == null || !camera.isActiveAndEnabled || !camera.CompareTag("MainCamera")) return;
+        var data = camera.GetUniversalAdditionalCameraData();
+        if (data == null) return;
+        if (BlurRenderPass != null)
+            data.scriptableRenderer.EnqueuePass(BlurRenderPass);
+        Debug.Log("beginCameraRendering");
+    }
+    public void SetAllImageVerticesDirty()
+    {
+        foreach (var background in m_BlurredBackgrounds)
+        {
+            background.SetVerticesDirty();
+        }
     }
 }
